58deabea33ea3a88171ade415741f5fe3b861766
Hasmukh-Mevada <35160907+Hasmukh-Mevada@users.noreply.github.com>
2020-02-20 16:15:39 +0530

    Update category-dropdown-woocommerce.php

--- category-dropdown-woocommerce.php
+++ category-dropdown-woocommerce.php
@@ -13,3 +13,9 @@ foreach( $product_categories as $category ){
 echo "</select>";
 ?>
 //https://d31wcbk3iidrjq.cloudfront.net/WHq7HCHQ-8B2BB6EA-8D18-4643-8AA4-56C70A32176E.jpg?w=40&h=40&q=80
+
+/*
+ * Woocommerce Remove excerpt from single product
+ */
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+add_action( 'woocommerce_single_product_summary', 'the_content', 20 );
